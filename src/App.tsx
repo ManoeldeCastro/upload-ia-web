a99a01ce1edacc898b8d3da37e5aa1f990a11ebc
manoel <manoeldiasmf@gmail.com>
2023-09-11 21:30:35 -0300

    init app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import './App.css';
+import { Button } from "./components/ui/button";
 
 export function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div>
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl">upload.ai</h1>
+        <div className="flex items-center gap-3">
+          <span className="text-muted-foreground text-sm">Desenvolvido com ❤️ no NLW da RocketSeat</span>
+          <Button>GitHub</Button>
+        </div>
+      </div>
+
+    </div>
   );
 }
 
